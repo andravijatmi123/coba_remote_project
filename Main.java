7dfad0c60fcca3225369a084ed128ba904375769
Andra Vijatmi <andravijatmi1933@gmail.com>
2024-06-06 12:51:31 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void Main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> pembunuhMap = new HashMap<>();
+        Set<String> korbanSet = new HashSet<>();
+
+        int maksBaris = 999;
+        int barisAwal = 0; 
+        int batasKarakterNama = 10;
+    }
+}
