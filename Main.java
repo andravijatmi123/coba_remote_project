5286ee076a73084b40df056066222c2a78beb92c
Andra Vijatmi <andravijatmi1933@gmail.com>
2024-06-06 12:59:16 +0700

    menambahkan fungsi loop dsb

--- Main.java
+++ Main.java
@@ -9,5 +9,42 @@ public class Main{
         int maksBaris = 999;
         int barisAwal = 0; 
         int batasKarakterNama = 10;
+
+        while (scanner.hasNextLine()) {
+            if (barisAwal >= maksBaris) {
+                System.out.println("Peringatan: Jumlah maksimum baris input telah tercapai.");
+                break;
+            }   
+
+            String line = scanner.nextLine();
+            barisAwal ++; 
+            if (line.trim().isEmpty()) {
+                break;
+            }
+            String[] names = line.split(" ");
+            String pembunuh = names[0];
+            String korban = names[1];
+            if (pembunuh.length() > batasKarakterNama || korban.length() > batasKarakterNama) {
+                System.out.println("Nama tidak boleh melebihi " + batasKarakterNama + " karakter.");
+                continue; 
+            }
+
+            if (Character.isLowerCase(pembunuh.charAt(0)) || Character.isLowerCase(korban.charAt(0))) {
+                System.out.println("Harus menggunakan huruf kapital di awal karakter");
+                continue;
+            }
+
+            if (!korbanSet.contains(korban)) {
+                pembunuhMap.put(pembunuh, pembunuhMap.getOrDefault(pembunuh, 0) + 1);
+                korbanSet.add(korban);
+            }
+        }
+        scanner.close();
+
+        for (String korban : korbanSet) {
+            pembunuhMap.remove(korban);
+        }
+        }
+
     }
 }
